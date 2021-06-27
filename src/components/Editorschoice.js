c54c3bef5172a20b6265fefcddfd713a79f0ee07
igalv <vilenskyigal@gmail.com>
2021-06-28 00:13:28 +0200

    editors choice component fix

--- src/components/Editorschoice.js
+++ src/components/Editorschoice.js
@@ -9,64 +9,97 @@ function Editorschoice() {
         </h2>
       </div>
       <div className="editor-items-container">
-        <div className="editor-item">
-          <div className="editor-img-container">
-            <img src="https://www.re-thinkingthefuture.com/wp-content/uploads/2021/01/A2797-10-must-have-gadgets-for-every-architect.jpg"></img>
-          </div>
-          <div className="editor-text">
+        <div className="editor-items">
+          <div className="editor-item">
+            <div className="editor-item-image">
+              <img src="https://www.re-thinkingthefuture.com/wp-content/uploads/2021/01/A2797-10-must-have-gadgets-for-every-architect.jpg"></img>
+            </div>
             <div className="editor-item-heading">
               <h3>Heading: This is heading</h3>
             </div>
-            <div>
+            <div className="author-date">
               <span>AUTHOR</span> <span> / </span>
-              <span>DATE</span>
+              <span>DATE</span>{" "}
             </div>
           </div>
-        </div>
-        <div className="editor-item">
-          <div className="editor-img-container">
-            <img src="https://assets.t3n.sc/news/wp-content/uploads/2020/03/office-gadgets-twelve-south-curve-620x388.jpg?auto=format%2Ccompress&fit=crop&h=388&ixlib=php-3.3.1&q=75&w=620"></img>
-          </div>
-          <div className="editor-text">
+          <div className="editor-item">
+            <div className="editor-item-image">
+              <img src="https://www.re-thinkingthefuture.com/wp-content/uploads/2021/01/A2797-10-must-have-gadgets-for-every-architect.jpg"></img>
+            </div>
             <div className="editor-item-heading">
               <h3>Heading: This is heading</h3>
             </div>
-            <div>
+            <div className="author-date">
               <span>AUTHOR</span> <span> / </span>
-              <span>DATE</span>
+              <span>DATE</span>{" "}
             </div>
           </div>
-        </div>
-        <div className="editor-item">
-          <div className="editor-img-container">
-            <img src="https://www.re-thinkingthefuture.com/wp-content/uploads/2021/01/A2797-10-must-have-gadgets-for-every-architect.jpg"></img>
-          </div>
-          <div className="editor-text">
+          <div className="editor-item">
+            <div className="editor-item-image">
+              <img src="https://www.werbeartikelgrosshandel.de/blog/wp-content/uploads/2019/05/Titelbild-die-10-coolsten-Technik-Gadget-Ideen-als-Werbegeschenk-1024x518.jpg"></img>
+            </div>
             <div className="editor-item-heading">
               <h3>Heading: This is heading</h3>
             </div>
-            <div>
+            <div className="author-date">
               <span>AUTHOR</span> <span> / </span>
-              <span>DATE</span>
+              <span>DATE</span>{" "}
             </div>
           </div>
-        </div>
-        <div className="editor-item">
-          <div className="editor-img-container">
-            <img src="https://cdn.prod.www.spiegel.de/images/54fffd82-c319-4bf4-a8a9-7a589d806492_w488_r1.778_fpx47.24_fpy44.96.jpg"></img>
-          </div>
-          <div className="editor-text">
+          <div className="editor-item">
+            <div className="editor-item-image">
+              <img src="https://www.re-thinkingthefuture.com/wp-content/uploads/2021/01/A2797-10-must-have-gadgets-for-every-architect.jpg"></img>
+            </div>
             <div className="editor-item-heading">
               <h3>Heading: This is heading</h3>
             </div>
-            <div>
+            <div className="author-date">
               <span>AUTHOR</span> <span> / </span>
-              <span>DATE</span>
+              <span>DATE</span>{" "}
             </div>
           </div>
         </div>
       </div>
     </div>
+
+    /*  <div className="editor-images">
+        <div className="editor-img-container">
+          <img src="https://www.re-thinkingthefuture.com/wp-content/uploads/2021/01/A2797-10-must-have-gadgets-for-every-architect.jpg"></img>
+          <img src="https://www.re-thinkingthefuture.com/wp-content/uploads/2021/01/A2797-10-must-have-gadgets-for-every-architect.jpg"></img>
+          <img src="https://www.re-thinkingthefuture.com/wp-content/uploads/2021/01/A2797-10-must-have-gadgets-for-every-architect.jpg"></img>
+          <img src="https://www.re-thinkingthefuture.com/wp-content/uploads/2021/01/A2797-10-must-have-gadgets-for-every-architect.jpg"></img>
+        </div>
+      </div>
+      <div className="editor-text">
+        <div className="editor-item-heading">
+          <h3>Heading: This is heading</h3>
+        </div>
+        <div className="author-date">
+          <span>AUTHOR</span> <span> / </span>
+          <span>DATE</span>
+          <div className="editor-item-heading">
+            <h3>Heading: This is heading</h3>
+          </div>
+          <div className="author-date">
+            <span>AUTHOR</span> <span> / </span>
+            <span>DATE</span>
+          </div>
+          <div className="editor-item-heading">
+            <h3>Heading: This is heading</h3>
+          </div>
+          <div className="author-date">
+            <span>AUTHOR</span> <span> / </span>
+            <span>DATE</span>
+          </div>
+          <div className="editor-item-heading">
+            <h3>Heading: This is heading</h3>
+          </div>
+          <div className="author-date">
+            <span>AUTHOR</span> <span> / </span>
+            <span>DATE</span>
+          </div>
+        </div>
+      </div> */
   );
 }
 
