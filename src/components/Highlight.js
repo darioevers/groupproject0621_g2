f97c0b434bffb7dc3bfbf66969c16049848f35a0
igalv <vilenskyigal@gmail.com>
2021-06-28 20:52:50 +0200

    highlights fix

--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -6,27 +6,67 @@ function Highlight() {
   Store.fetchData();
 
   const highlight = Store.dataNew.map((item, i) => {
-    if (i < 3) {
+    if (i < 1) {
       return (
-        <li key={i}>
-          <div className="highlight-left">
+        <div className="highlight">
+          <div key={i} className="highlight-left">
             <div className="highlight-left-img">
-              <a href={item.url} target="_blank">
-                <img className="img-res" src={item.urlToImage} />
-              </a>
+              {
+                <a href={item.url} target="_blank">
+                  <img src={item.urlToImage} />
+                </a>
+              }
             </div>
             <div className="highlight-left-text">
               <h4>CATEGORY</h4>
-              <div>
+              <div className="highlight-heading-left">
                 <h3>{item.title}</h3>
               </div>
-              <div>
-                <span>AUTHOR{item.author} </span> <span> / </span>
-                <span>DATE{item.publishedAt} </span>
+              <div className="author-date-left">
+                <span>{item.author} </span> <span> / </span>
+                <span>{item.publishedAt} </span>
               </div>
             </div>
           </div>
-        </li>
+
+          <div key={i} className="highlight-middle">
+            <div className="highlight-middle-img">
+              {
+                <a href={item.url} target="_blank">
+                  <img src={item.urlToImage} />
+                </a>
+              }
+            </div>
+            <div className="highlight-middle-text">
+              <h4>CATEGORY</h4>
+              <div className="highlight-heading-middle">
+                <h3>{item.title}</h3>
+              </div>
+              <div className="author-date-middle">
+                <span>{item.author} </span> <span> / </span>
+                <span>{item.publishedAt} </span>
+              </div>
+            </div>
+          </div>
+
+          <div key={i} className="highlight-right">
+            <div className="highlight-right-img">
+              <a href={item.url} target="_blank">
+                <img src={item.urlToImage} />
+              </a>
+            </div>
+            <div className="highlight-right-text">
+              <h4>CATEGORY</h4>
+              <div className="highlight-heading-right">
+                <h3>{item.title}</h3>
+              </div>
+              <div className="author-date-right">
+                <span>{item.author} </span> <span> / </span>
+                <span>{item.publishedAt} </span>
+              </div>
+            </div>
+          </div>
+        </div>
       );
     }
   });
