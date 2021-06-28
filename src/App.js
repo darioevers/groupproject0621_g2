5d7ef8344db09bbb934c42a05ff9c0a5f078f297
meriemkhadraoui <miyakhad@gmail.com>
2021-06-28 08:43:57 +0200

    add api use context hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "./sass/main.scss";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Lastest from "./components/Latest";
+import { Store, storContext } from "./context";
 
 function App() {
+  const store = useContext(Store);
   const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div>
+    <storContext.Provider value={store}>
       <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Lastest />
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-    </div>
+    </storContext.Provider>
   );
 }
 
---
+++ src/components/Latest.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { Store } from "../context";
+const Lastest = () => {
+  const store = useContext(Store);
+  console.log(Store.dataNew);
+  Store.fetchData();
+  const newsList = Store.dataNew.map((item, i) => (
+    <li key={i}>
+      <h2>{item.title} </h2>
+      <h3>{item.content}</h3>
+      <h5>{item.author}</h5>
+      <h6>{item.publishedAt} </h6>
+      <img src={item.urlToImage} />
+      <a href={item.url} target="_blank">
+        <button>Show more</button>
+      </a>
+    </li>
+  ));
+  return (
+    <div className="news-box">
+      <ul>{newsList}</ul>
+    </div>
+  );
+};
+export default Lastest;
