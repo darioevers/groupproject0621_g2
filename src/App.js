08fa690d14e5dbecd8c6fba601cf94af5daf233a
meriemkhadraoui <miyakhad@gmail.com>
2021-06-28 12:23:19 +0200

    new add

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Lastest from "./components/Latest";
 import { Store, storContext } from "./context";
+import Highlight from "./components/Highlight";
+import Editorschoice from "./components/Editorschoice";
 
 function App() {
   const store = useContext(Store);
@@ -11,8 +13,12 @@ function App() {
   return (
     <storContext.Provider value={store}>
       <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <Lastest />
+
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+
+      <Highlight />
+      <Editorschoice />
+      <Lastest />
     </storContext.Provider>
   );
 }
