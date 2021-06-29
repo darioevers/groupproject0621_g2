2f6349564cd851f5505febbad6cbbfb1c64265d8
ahmed nabbaa <ahmed.nabaa@outlook.com>
2021-06-29 14:13:33 +0200

    second

--- src/App.js
+++ src/App.js
@@ -8,35 +8,28 @@ import { Store, storContext } from "./context";
 import Highlight from "./components/Highlight";
 import Editorschoice from "./components/Editorschoice";
 import Cta from "./components/Cta";
+import Secondblock from "./components/Secondblock";
 import Aboutfooter from "./components/Aboutfooter";
 import Mainfooter from "./components/Mainfooter";
 import Spotlight from "./components/Spotlight";
-
-
-
 function App() {
   const store = useContext(Store);
-
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <storContext.Provider value={store}>
-      
       <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-
       <Highlight />
-
       <Editorschoice />
-
       <Cta />
       <Latest2 />
       <Spotlight />
       {/*  <Lastest /> */}
+      <Spotlight />
+      <Secondblock />
       <Aboutfooter />
       <Mainfooter />
     </storContext.Provider>
   );
 }
-
 export default App;
