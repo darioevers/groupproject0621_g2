c5c11c46c6d1c3fa628e7cc187984b744c2a0b2d
Dario Evers <darioevers@gmail.com>
2021-06-29 10:41:16 +0200

    aboutfooter added

--- src/components/Aboutfooter.js
+++ src/components/Aboutfooter.js
@@ -2,8 +2,16 @@ import React from "react";
 export default function Aboutfooter() {
   return (
     <div class="aboutFooter">
-        <div class="aboutFooter-left"></div>
-        <div class="aboutFooter-right"></div>
+        <div class="aboutFooter-left">
+            <h4>ABOUT US</h4>
+            <h1>TECH NEWS</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+            <p>Email: TECHNEWS@TECHNEWS.COM</p>
+            <p>Phone: 123456789</p>
+        </div>
+        <div class="aboutFooter-right">
+            {/* API BELONGS HERE */}
+        </div>
     </div>
   );
 }
