2a5e25f929f16741fe9e8f066758eb78a7fd63dd
Dario Evers <darioevers@gmail.com>
2021-06-30 22:24:13 +0200

    fixed highlight

--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -1,32 +1,29 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Store, StoreContext } from "../context";
-
 function Highlight() {
   const contextObj = useContext(StoreContext);
-
   console.log(contextObj.newArr);
   const items = contextObj.newArr.map((item, i) => {
     if (i > 3 && i < 7) {
       const dateArr = item.publishedAt.split("").slice(0, 10).join("");
-      console.log(dateArr);
-
       return (
         <div key={i} className="highlight">
           <div className="highlight-left-img">
             <a href={item.url} target="_blank">
               <img src={item.urlToImage} />
             </a>
-          </div>
-          <div className="highlight-right-text">
-            <h4>CATEGORY</h4>
-            <div className="highlight-heading-left">
-              <a href={item.url} target="_blank">
-                <h3>{item.title}</h3>
-              </a>
-            </div>
-            <div className="author-date-left">
-              <span>{item.author} </span> <span> / </span>
-              <span>{dateArr}</span>
+
+            <div className="highlight-text">
+              <h4>HIGHLIGHT</h4>
+              <div className="highlight-heading">
+                <a href={item.url} target="_blank">
+                  <h3>{item.title}</h3>
+                </a>
+              </div>
+              <div className="author-date-left">
+                <span>{item.author} </span> <span> / </span>
+                <span>{dateArr}</span>
+              </div>
             </div>
           </div>
         </div>
@@ -35,5 +32,4 @@ function Highlight() {
   });
   return <div className="highlight"> {items}</div>;
 }
-
 export default Highlight;
