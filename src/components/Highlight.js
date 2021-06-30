4d46f9f78c070dedbc4d7c58ff7494dec23f1b62
igalv <vilenskyigal@gmail.com>
2021-06-30 19:09:44 +0200

    highlight component styling

--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -8,20 +8,20 @@ function Highlight() {
   const items = contextObj.newArr.map((item, i) => {
     if (i > 3 && i < 7) {
       return (
-        <div key={i} className="highlight-left">
+        <div key={i} className="highlight">
           <div className="highlight-left-img">
             <a href={item.url} target="_blank">
               <img src={item.urlToImage} />
             </a>
-          </div>
-          <div className="highlight-right-text">
-            <h4>CATEGORY</h4>
-            <div className="highlight-heading-left">
-              <h3>{item.title}</h3>
-            </div>
-            <div className="author-date-left">
-              <span>{item.author} </span> <span> / </span>
-              <span>{item.publishedAt} </span>
+            <div className="highlight-text">
+              <h4>TECH NEWS HIGHLIGHT</h4>
+              <div className="highlight-heading">
+                <h3>{item.title}</h3>
+              </div>
+              <div className="author-date-left">
+                <span>{item.author} </span> <span> / </span>
+                <span>{item.publishedAt} </span>
+              </div>
             </div>
           </div>
         </div>
